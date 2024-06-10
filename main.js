e523b91a7bd5eec0fcb3268a38147599784206a2
smg02 <syedmdghouse02@gmail.com>
2024-06-10 19:32:28 +0530

    half js done

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const myLibrary = [];
-const bookTitle = document.querySelector("")
+const container = document.querySelector(".cards-container");
+
+const changeReadStatus = () => {
+    console.log("works")
+}
+
+// const bookTitle = document.querySelector(".book-name");
+// const bookStatus = document.querySelector("#status");
+// const bookDelete = document.querySelector("#delete");
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -12,7 +20,6 @@ const addBookToLibrary = (title, author, pages) => {
     myLibrary.push({title, author, pages})
 }
 
-
 addBookToLibrary('hp', 'jk', 324);
 
 addBookToLibrary('hp2', 'jk', 652);
@@ -20,13 +27,33 @@ addBookToLibrary('hp2', 'jk', 652);
 addBookToLibrary('hp3', 'jk', 545);
 
 myLibrary.forEach((element) => {
-    console.log(element)
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "card");
+
+    const bookName = document.createElement("span");
+    bookName.setAttribute("class", "book-name");
+    bookName.textContent = `${element.title}`;
+
+    const bookStatus = document.createElement("button");
+    bookStatus.setAttribute("id", "status");
+    bookStatus.addEventListener(changeReadStatus);
+    bookStatus.textContent = "Read";
+    
+    const bookDelete = document.createElement("button");
+    bookDelete.setAttribute("id", "delete");
+    bookDelete.textContent = "Delete";
+    
+    container.append(div);
+    div.append(bookName);
+    div.append(bookStatus);
+    div.append(bookDelete);
 })
-// console.log(myLibrary[1])
+
 
 // Book.prototype.showStats = function() {
-//     return `${this.title}, ${this.author}, ${this.pages} and you have ${this.read}!`
-// }
+    //     return `${this.title}, ${this.author}, ${this.pages} and you have ${this.read}!`
+    // }
 
 // const theHobbit = new Book("theHobbit", "John Cena", 934, "not read")
 // console.log(theHobbit.showStats())
