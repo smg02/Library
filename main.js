e7a5b5c4f1f07aaabe2e1dd38c096fae33c0b1e0
Mohammed Ghouse <76863172+smg02@users.noreply.github.com>
2024-06-11 18:17:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,116 @@
-const myLibrary = [];
+const wasMyLibrary = [];
+
+// HTML Elements
 const container = document.querySelector(".cards-container");
+const dialog = document.querySelector("dialog");
+const openDialog = document.querySelector("#open");
+const closeDialog = document.querySelector("#close");
+const add = document.querySelector("#add");
+const form = document.querySelector("form");
 
-const changeReadStatus = () => {
-    console.log("works")
-}
-
-// const bookTitle = document.querySelector(".book-name");
-// const bookStatus = document.querySelector("#status");
-// const bookDelete = document.querySelector("#delete");
-
-function Book (title, author, pages, read) {
+// Object / Constructor
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-const addBookToLibrary = (title, author, pages) => {
-    myLibrary.push({title, author, pages})
+
+// Fuctions to Add Items in the Library Array
+const addBookToLibrary = (title, author, pages, read) => {
+    wasMyLibrary.push({ title, author, pages, read })
+    displayBooks()
 }
 
-addBookToLibrary('hp', 'jk', 324);
 
-addBookToLibrary('hp2', 'jk', 652);
-
-addBookToLibrary('hp3', 'jk', 545);
-
-myLibrary.forEach((element) => {
-
-    const div = document.createElement("div");
-    div.setAttribute("class", "card");
-
-    const bookName = document.createElement("span");
-    bookName.setAttribute("class", "book-name");
-    bookName.textContent = `${element.title}`;
-
-    const bookStatus = document.createElement("button");
-    bookStatus.setAttribute("id", "status");
-    bookStatus.addEventListener(changeReadStatus);
-    bookStatus.textContent = "Read";
-    
-    const bookDelete = document.createElement("button");
-    bookDelete.setAttribute("id", "delete");
-    bookDelete.textContent = "Delete";
-    
-    container.append(div);
-    div.append(bookName);
-    div.append(bookStatus);
-    div.append(bookDelete);
+// Dialog Evenet Listeners
+openDialog.addEventListener("click", () => {
+    dialog.showModal()
 })
 
+closeDialog.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog.close()
+})
 
-// Book.prototype.showStats = function() {
-    //     return `${this.title}, ${this.author}, ${this.pages} and you have ${this.read}!`
-    // }
+add.addEventListener("click", (event) => {
+    if (document.querySelector("#book-title").value == "") {
+    } else if (document.querySelector("#author").value == "") {
+    } else if (document.querySelector("#page-num").value == "") {
+    } else {
+        let userInputTitle = document.querySelector("#book-title").value;
+        let bookAuthorValue = document.querySelector("#author").value;
+        let bookPageValue = document.querySelector("#page-num").value;
+        let bookStatusValue = document.querySelector("#book-status").value;
+        addBookToLibrary(userInputTitle, bookAuthorValue, bookPageValue, bookStatusValue);
+        event.preventDefault();
+        form.reset();
+        dialog.close();
+    }
+}, false)
 
-// const theHobbit = new Book("theHobbit", "John Cena", 934, "not read")
-// console.log(theHobbit.showStats())
 
-// console.log(Object.getPrototypeOf(theHobbit))
-// console.log(Book.prototype)
+// Managing the Cards (Clearing Old cards, Read Status Change and Delete)
+const displayBooks = () => {
 
-// console.log(theHobbit.valueOf())
+    clearScreen();
+
+    wasMyLibrary.forEach((element, index) => {
+
+        const div = document.createElement("div");
+        div.setAttribute("class", "card");
+
+        const bookName = document.createElement("span");
+        bookName.setAttribute("class", "book-name");
+        bookName.textContent = `${element.title}`;
+
+        const bookAuthor = document.createElement("span");
+        bookName.setAttribute("class", "book-author");
+        bookAuthor.textContent = `${element.author}`;
+
+        const bookPages = document.createElement("span");
+        bookPages.setAttribute("class", "book-pages");
+        bookPages.textContent = `${element.pages}`;
+
+        const bookStatus = document.createElement("button");
+        bookStatus.setAttribute("id", "status");
+        bookStatus.textContent = `${element.read}`;
+        bookStatus.addEventListener("click", () => {
+            if (element.read === "Read") {
+                element.read = "Unread";
+                bookStatus.textContent = `${element.read}`;
+                bookStatus.style.backgroundColor = "green";
+            } else {
+                element.read = "Read";
+                bookStatus.textContent = `${element.read}`;
+                bookStatus.style.backgroundColor = "red";
+            }
+        });
+
+        const bookDelete = document.createElement("button");
+        bookDelete.setAttribute("id", "delete");
+        bookDelete.addEventListener("click", deleteItem)
+        bookDelete.textContent = "Delete";
+
+        bookDelete.dataset.id = `${index}`
+        bookStatus.dataset.id = `${index}`
+
+        container.append(div);
+        div.append(bookName);
+        div.append(bookAuthor);
+        div.append(bookPages);
+        div.append(bookStatus);
+        div.append(bookDelete);
+    })
+}
+
+const deleteItem = (event) => {
+    let id = event.target.dataset.id;
+    wasMyLibrary.splice(id, 1)
+    displayBooks();
+}
+
+const clearScreen = () => {
+    document.querySelectorAll(".card").forEach((element) => element.remove())
+} 
